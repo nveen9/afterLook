d10584eb528e0f2a7312bc6aa86c16068b9a6629
donut9 <naveenperera9@gmail.com>
2023-03-01 15:44:26 +0530

    drag flatlist

--- src/screens/Testt.js
+++ src/screens/Testt.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+// import { Text, TouchableOpacity, StyleSheet } from "react-native";
+// import DraggableFlatList, {
+//   ScaleDecorator,
+// } from "react-native-draggable-flatlist";
 
 const Testt = () => {
+//   const [data, setData] = useState([
+//     {
+//       id: "1",
+//       title: "First Item",
+//     },
+//     {
+//       id: "2",
+//       title: "Second Item",
+//     },
+//     {
+//       id: "3",
+//       title: "Third Item",
+//     },
+//   ]);
 
-}
+//   const renderItem = ({ item, drag, isActive }) => (
+//     <ScaleDecorator>
+//       <TouchableOpacity
+//         onLongPress={drag}
+//         disabled={isActive}
+//         style={styles.item}
+//       >
+//         <Text>{item.title}</Text>
+//       </TouchableOpacity>
+//     </ScaleDecorator>
+//   );
+
+//   return (
+//     <DraggableFlatList
+//       data={data}
+//       onDragEnd={({ data }) => {
+//         setData(data);
+//         console.log("End Data: ", data);
+//       }}
+//       keyExtractor={(item) => item.id}
+//       renderItem={renderItem}
+//     />
+//   );
+};
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#D6AD60",
+//   },
+//   item: {
+//     backgroundColor: "#f9c2ff",
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+// });
 
 export default Testt;
