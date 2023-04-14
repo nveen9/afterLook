e989fb39d83e8a9b209614c29971420e06851368
donut9 <naveenperera9@gmail.com>
2023-04-14 20:35:12 +0530

    background service

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, SafeAreaView, Text, View, StatusBar } from "react-native";
+import { StyleSheet, SafeAreaView, Text, View, StatusBar, Switch, Linking, PermissionsAndroid, DevSettings } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { parse, stringify, toJSON, fromJSON } from 'flatted';
+import { accelerometer, gyroscope, setUpdateIntervalForType, SensorTypes } from 'react-native-sensors';
+import BackgroundService from 'react-native-background-actions';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import auth from '@react-native-firebase/auth';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { useIsFocused } from '@react-navigation/native';
+
+setUpdateIntervalForType(SensorTypes.accelerometer, 1000);
+setUpdateIntervalForType(SensorTypes.gyroscope, 1000);
 
 const Home = ({ navigation }) => {
   const [isSignedIn, setIsSignedIn] = useState(false);
-  const isFocused = useIsFocused();
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [subscriptionA, setSubscriptionA] = useState(null);
+  const [subscriptionG, setSubscriptionG] = useState(null);
+  const [accelerometerData, setAccelerometerData] = useState({ x: 0, y: 0, z: 0, timestamp: 0 });
+  const [gyroscopeData, setGyroscopeData] = useState({ x: 0, y: 0, z: 0, timestamp: 0 });
 
   useEffect(() => {
     if (!isSignedIn) {
       auth().onAuthStateChanged(user => {
         if (!user) {
-            console.log('User not found!');
-            setIsSignedIn(false);
+          console.log('User not found!');
+          setIsSignedIn(false);
         } else {
           console.log('User found!');
           // navigation.navigate('Home');
@@ -24,36 +33,135 @@ const Home = ({ navigation }) => {
     }
   }, [isSignedIn]);
 
-  const logout = async () => {
-    const googleUser = await GoogleSignin.isSignedIn();
+  useEffect(() => {
+    if (!isEnabled) {
+      const getEnab = async () => {
+        try {
+          const enab = await AsyncStorage.getItem("enab");
+          if (enab !== null) {
+            const enabb = JSON.parse(enab);
+            console.log('ee', enabb);
+            setIsEnabled(enabb);
+          } else {
+            console.log("No saved state");
+          }
+        } catch (error) {
+          console.log("Error getting the state", error);
+        }
+      };
+      getEnab();
+    }
+  }, [isEnabled]);
 
-    if(googleUser) {
-      try{
-        await GoogleSignin.revokeAccess();
-        await GoogleSignin.signOut();
-      }catch(err){
-        console.log(err);
-      }     
-    }else {
-      auth().signOut().then(() => {
-        console.log('Signout Success');
-      }).catch((error) => {
-        console.log(error);
-      });
-    } 
+  //Background task
+  const sleep = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
+
+  BackgroundService.on('expiration', () => {
+    console.log('I am being closed :(');
+  });
+
+  // You can do anything in your task such as network requests, timers and so on,
+  // as long as it doesn't touch UI. Once your task completes (i.e. the promise is resolved),
+  // React Native will go into "paused" mode (unless there are other tasks running,
+  // or there is a foreground app).
+  const veryIntensiveTask = async (taskDataArguments) => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+        {
+          title: 'After Look App Notification Permission',
+          message:
+            'After Look App needs access to your Notification ' +
+            'In order to run the app background.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        const { delay } = taskDataArguments;
+        await new Promise(async (resolve) => {
+          const subA = accelerometer.subscribe(({ x, y, z, timestamp }) => {
+            setAccelerometerData({ x, y, z, timestamp });
+            console.log(timestamp);
+          });
+          setSubscriptionA(subA);
+          const subG = gyroscope.subscribe(({ x, y, z, timestamp }) => {
+            setGyroscopeData({ x, y, z, timestamp });
+          });
+          setSubscriptionG(subG);
+          await BackgroundService.updateNotification({ taskDesc: 'Reading' }); // Only Android, iOS will ignore this call      
+          await sleep(delay);
+        });
+      } else {
+        console.log('Permission denied');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const options = {
+    taskName: 'After Look',
+    taskTitle: 'Getting Sensor Data',
+    taskDesc: 'Reading',
+    taskIcon: {
+      name: 'ic_launcher',
+      type: 'mipmap',
+    },
+    color: '#ff00ff',
+    linkingURI: 'backScheme://chat/jane', // See Deep Linking for more info
+    parameters: {
+      delay: 1000,
+    },
+  };
+  //
+
+  const toggleSwitch = async () => {
+    if (isEnabled) {
+      if (subscriptionA) {
+        subscriptionA.unsubscribe();
+        setSubscriptionA(null);
+      }
+
+      if (subscriptionG) {
+        subscriptionG.unsubscribe();
+        setSubscriptionG(null);
+      }
+      try {
+        await AsyncStorage.setItem('enab', JSON.stringify(!isEnabled));
+        await BackgroundService.stop();
+        DevSettings.reload();
+      } catch (error) {
+        console.log('Error', error);
+      }
+    } else {
+      try {
+        await AsyncStorage.setItem('enab', JSON.stringify(!isEnabled));
+        await BackgroundService.start(veryIntensiveTask, options);
+      } catch (error) {
+        console.log('Error', error);
+      }
+
+    }
+
+    setIsEnabled(previousState => !previousState);
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar translucent backgroundColor="#2A2E30" />
-      <Text style={styles.signUpText}>Email {isSignedIn ? auth().currentUser.email : 'No User Signed In'}</Text>
+      <Switch
+        style={{ transform: [{ scaleX: 2 }, { scaleY: 2 }], margin: 10 }}
+        trackColor={{ false: '#767577', true: '#FFD700' }}
+        thumbColor={isEnabled ? '#2A2E30' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
       <View style={styles.devider}></View>
       <TouchableOpacity title='Signup' onPress={() => navigation.navigate('Signup')}>
         <Text style={styles.signUpText}>Signup</Text>
-      </TouchableOpacity>
-      <View style={styles.devider}></View>
-      <TouchableOpacity title='Logout' onPress={logout}>
-        <Text style={styles.signUpText}>Logout</Text>
       </TouchableOpacity>
     </SafeAreaView>
   )
--- src/screens/Testt.js
+++ src/screens/Testt.js
@@ -1,31 +1,134 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking, PermissionsAndroid } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {parse, stringify, toJSON, fromJSON} from 'flatted';
 import { accelerometer, gyroscope, setUpdateIntervalForType, SensorTypes } from 'react-native-sensors';
+import BackgroundService from 'react-native-background-actions';
 
-setUpdateIntervalForType(SensorTypes.accelerometer, 500);
-setUpdateIntervalForType(SensorTypes.gyroscope, 500);
+setUpdateIntervalForType(SensorTypes.accelerometer, 1000);
+setUpdateIntervalForType(SensorTypes.gyroscope, 1000);
 
 const Testt = () => {
 
-    const [set, isSet] = useState(true);
+    const [enb, setEnb] = useState(true);
+    const [subscriptionA, setSubscriptionA] = useState(null);
+    const [subscriptionG, setSubscriptionG] = useState(null);
     const [accelerometerData, setAccelerometerData] = useState({ x: 0, y: 0, z: 0, timestamp: 0 });
     const [gyroscopeData, setGyroscopeData] = useState({ x: 0, y: 0, z: 0, timestamp: 0 });
 
     useEffect(() => {
-        if(!set){
-        const subscriptionA = accelerometer.subscribe(({ x, y, z, timestamp }) => {
-            setAccelerometerData({ x, y, z, timestamp });
-        });
-        const subscriptionG = gyroscope.subscribe(({ x, y, z, timestamp }) => {
-            setGyroscopeData({ x, y, z, timestamp });
-        });
-
-        return () => {
-            subscriptionA.unsubscribe();
-            subscriptionG.unsubscribe();
-        }
-    }
+        const getEnab = async () => {
+            try {
+              const subA = await AsyncStorage.getItem("subA");
+              const subG = await AsyncStorage.getItem("subG");
+              if (subA !== null && subG !== null) {
+                const subAA = parse(subA);
+                const subGG = parse(subG);
+                console.log('s',subAA)
+              } else {
+                console.log("No saved state");
+              }
+            } catch (error) {
+              console.log("Error getting the state", error);
+            }
+          };
+          getEnab();
     }, []);
+
+    //Background task
+    const sleep = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
+
+    BackgroundService.on('expiration', () => {
+        console.log('I am being closed :(');
+    });
+
+    // You can do anything in your task such as network requests, timers and so on,
+    // as long as it doesn't touch UI. Once your task completes (i.e. the promise is resolved),
+    // React Native will go into "paused" mode (unless there are other tasks running,
+    // or there is a foreground app).
+    const veryIntensiveTask = async (taskDataArguments) => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+                {
+                    title: 'After Look App Notification Permission',
+                    message:
+                        'After Look App needs access to your Notification ' +
+                        'In order to run the app background.',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                const { delay } = taskDataArguments;
+                await new Promise(async (resolve) => {
+                    const subA = accelerometer.subscribe(({ x, y, z, timestamp }) => {
+                        setAccelerometerData({ x, y, z, timestamp });
+                        console.log(timestamp);
+                    });
+                    setSubscriptionA(subA);
+                    const subG = gyroscope.subscribe(({ x, y, z, timestamp }) => {
+                        setGyroscopeData({ x, y, z, timestamp });
+                    });
+                    setSubscriptionG(subG);
+                    try {
+                        await AsyncStorage.setItem('subA', stringify(subA));
+                        await AsyncStorage.setItem('subG', stringify(subG));
+                    } catch (error) {
+                        console.log('Error saving state', error);
+                    }
+                    await BackgroundService.updateNotification({ taskDesc: 'Reading' }); // Only Android, iOS will ignore this call      
+                    await sleep(delay);
+                });
+            } else {
+                console.log('Permission denied');
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const options = {
+        taskName: 'After Look',
+        taskTitle: 'Getting Sensor Data',
+        taskDesc: 'Reading',
+        taskIcon: {
+            name: 'ic_launcher',
+            type: 'mipmap',
+        },
+        color: '#ff00ff',
+        linkingURI: 'backScheme://chat/jane', // See Deep Linking for more info
+        parameters: {
+            delay: 1000,
+        },
+    };
+
+    const startBack = async () => {
+        await BackgroundService.start(veryIntensiveTask, options);
+    }
+
+    const stopBack = async () => {
+        if (subscriptionA) {
+            subscriptionA.unsubscribe();
+            setSubscriptionA(null);
+        }
+
+        if (subscriptionG) {
+            subscriptionG.unsubscribe();
+            setSubscriptionG(null);
+        }
+        await BackgroundService.stop();
+    }
+
+    Linking.addEventListener('url', handleOpenURL);
+
+    function handleOpenURL(evt) {
+        // Will be called when the notification is pressed
+        console.log(evt.url);
+        // do something
+    }
+    // 
 
     return (
         <View style={styles.container}>
@@ -39,6 +142,8 @@ const Testt = () => {
             <Text>y: {gyroscopeData.y}</Text>
             <Text>z: {gyroscopeData.z}</Text>
             <Text>time: {gyroscopeData.timestamp}</Text>
+            <Button title="Start" onPress={startBack} />
+            <Button title="Stop" onPress={stopBack} />
         </View>
     );
 }
@@ -57,6 +162,6 @@ const styles = StyleSheet.create({
     }
 });
 
-  
+
 
 export default Testt;
